92365096975b97a557445f91d4ffeb48bafc34d3
Rene Elizondo <43520443+relizondo2018@users.noreply.github.com>
2023-10-14 13:49:33 -0600

    feat: Add User Registration

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required'
+        ]);
+
+        $newUser = User::create($data);
+
+        Auth::login($newUser);
+
+        return redirect('/')
+            ->with('success', 'Account created successfully');
+    }
+   
+}
