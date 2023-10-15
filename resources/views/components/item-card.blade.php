c941ea5aafa10784c96351f60add4c074522a154
Rene Elizondo <43520443+relizondo2018@users.noreply.github.com>
2023-10-14 23:15:02 -0600

    feat: create iteam card component

---
+++ resources/views/components/item-card.blade.php
@@ -0,0 +1,54 @@
+<div class="flex flex-col ">
+            <div class="bg-white shadow-md  rounded-3xl p-4">
+                <div class="flex-none lg:flex">
+                    <div class=" h-full w-full lg:h-48 lg:w-48   lg:mb-0 mb-3">
+                        <img src="{{ $image }}"
+                            alt="Just a flower" class=" w-full  object-scale-down lg:object-cover  lg:h-48 rounded-2xl">
+                    </div>
+                    <div class="flex-auto ml-3 justify-evenly py-2">
+                        <div class="flex flex-wrap ">
+                            <div class="w-full flex-none text-xs text-blue-700 font-medium ">
+                                {{ ucfirst($condition) }}
+                            </div>
+                            <h2 class="flex-auto text-lg font-medium">{{ ucfirst($name) }}</h2>
+                        </div>
+                        <p class="mt-3"></p>
+                        <div class="flex py-4  text-sm text-gray-500">
+                            <div class="flex-1 inline-flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3 text-gray-400" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z">
+                                    </path>
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                                </svg>
+                                <p class=""> {{ $location }} </p>
+                            </div>
+                            <div class="flex-1 inline-flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 text-gray-400" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                </svg>
+                                <p class="">Ends at: {{ $endsat }} </p>
+                            </div>
+                        </div>
+                        <div class="flex p-4 pb-2 border-t border-gray-200 "></div>
+                        <div class="flex space-x-3 text-sm font-medium">
+                            <div class="flex-auto flex space-x-3">
+                                <div class="flex-1 inline-flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                                <p class="text-gray-400"> {{ number_format($bid, 2) }} </p>
+                            </div>
+                            </div>
+                            <button
+                                class="mb-2 md:mb-0 bg-gray-900 px-5 py-2 shadow-sm tracking-wider text-white rounded-full hover:bg-gray-800"
+                                type="button" aria-label="like">Place your bid</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
