cf21228768d3904688ca5f768e293d85f7389925
Rene Elizondo <43520443+relizondo2018@users.noreply.github.com>
2023-10-14 23:15:17 -0600

    upd: add the item seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,12 +11,24 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+    {        
+        \App\Models\User::factory(100)->create();
 
-         \App\Models\User::factory()->create([
-             'name' => 'admin',
-             'email' => 'admin@example.com',
-         ]);
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+        ]);
+
+        $itemOwners = \App\Models\User::inRandomOrder()->take(50)->get();
+
+        foreach ($itemOwners as $owner) {
+            for ($i=0; $i < rand(1, 5); $i++) {
+                \App\Models\Item::factory()->create([
+                    'user_id' => $owner->id
+                ]);
+            }
+        }
+
     }
+
 }
