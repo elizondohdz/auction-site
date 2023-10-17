8043705c93a43bcbbda5dedd666a6791b12611c8
Rene Elizondo <43520443+relizondo2018@users.noreply.github.com>
2023-10-16 21:48:24 -0600

    feat: refactor and add scope filtering

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Item extends Model
 {
@@ -15,5 +18,30 @@ class Item extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function bids(): HasMany
+    {
+        return $this->hasMany(Bid::class);
+    }
+
+    public function scopeFilter(Builder|QueryBuilder $query, array $filters): Builder|QueryBuilder
+    {
+        return $query->when($filters['search'] ?? null, function($query, $search){
+            $query->where(function($query) use($search) {
+                $query->where('name', 'like', '%'. $search .'%')
+                ->orWhere('description', 'like', '%'. $search .'%')
+                ->orWhere('location', 'like', '%'. $search .'%');
+                });
+            })
+            ->when($filters['name'] ?? null, function($query, $name){
+                $query->where('name', 'like', '%'. $name .'%');
+            })
+            ->when($filters['condition'] ?? null, function($query, $condition){
+                $query->where('condition', $condition);
+            })
+            ->when($filters['location'] ?? null, function($query, $location){
+                $query->where('location', 'like' ,'%' . $location . '%');
+            });
     }
 }
