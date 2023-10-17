8a32ffe394abc88ae25284557aec94c1761e05e0
Rene Elizondo <43520443+relizondo2018@users.noreply.github.com>
2023-10-16 21:48:01 -0600

    feat: Bid model

---
+++ app/Models/Bid.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Bid extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['item_id', 'user_id', 'amount'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function item(): BelongsTo
+    {
+        return $this->belongsTo(Item::class);
+    }
+
+}
